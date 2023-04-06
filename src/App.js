635f9285e3e09eaba85fa010512c779a299a423e
jarenella <jamesa122333@gmail.com>
2023-04-06 11:35:08 -0700

    cleaning up code organization with modularized css files for every component and defualt browser css resets in reset.css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './reset.css';
 import Header from './components/Header';
 
 function App() {
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,45 +1,16 @@
+import './Header.css';
 import React from "react";
 const logo = "https://cdn-icons-png.flaticon.com/512/2579/2579272.png";
 
 function NavBar() {
-    const headerCSS = {
-        position: "fixed",
-        width: "100%",
-        zIndex: "3",
-        top: "0px",
-        backgroundColor: "rgb(39 39 42)",
-        display: "flex",
-    }
-    const logoCSS = {
-        width: "50px",
-        height: "50px",
-        display: "inline",
-        margin: "1rem",
-        alignItems: "center",
-    }
-    const navbarCSS = {
-        display: "inline-flex",
-    }
-    const buttonsCSS = {
-        color: "rgb(255 255 255)",
-        padding: "0.5rem",
-        borderRadius: "0.375rem",
-        alignItems: "center",
-        display: "inline-flex",
-        textDecoration: "none",
-        marginTop: "8%",
-        marginBottom: "8%",
-        //on-hover & on-focus & font family styling can be found in App.css
-    }
-    
     return (
-        <header style={headerCSS} className="App-header">
-            <img id="navbar-icon" style={logoCSS} src={logo} className="App-logo" alt="logo" />
-            <nav style={navbarCSS}>
-                <a style={buttonsCSS} className="navbar-btn" href="#">Home</a>
-                <a style={buttonsCSS} className="navbar-btn" href="#">Projects</a>
-                <a style={buttonsCSS} className="navbar-btn" href="#">About</a>
-                <a style={buttonsCSS} className="navbar-btn" href="#">Contact</a>
+        <header className="header">
+            <img id="navbar-icon" src={logo} className="App-logo" alt="logo" />
+            <nav class="navbar">
+                <a className="navbar-btn" href="test">Home</a>
+                <a className="navbar-btn" href="test">Projects</a>
+                <a className="navbar-btn" href="test">About</a>
+                <a className="navbar-btn" href="test">Contact</a>
             </nav>
         </header>
     )
