c72937e589166f859c6f83e80d188d4f1cafd090
jarenella <jamesa122333@gmail.com>
2023-04-28 14:21:03 -0700

    developed bar component further

--- src/components/Bar/index.js
+++ src/components/Bar/index.js
@@ -4,8 +4,13 @@ import React from "react";
 function Bar() {
     return (
         <section className="bar-bg">
-            <h2 className="bar-title">Header</h2>
-            <p className="bar-body">Body</p>
+            <img className="abt-img" alt="filler" src="https://www.sfspca.org/wp-content/uploads/2023/04/11a9ff30-6e7e-438c-a547-f27cc5fc4f01.jpg"></img>
+            
+            <div className="bar-txt-div">
+                <h2 className="bar-title">Header</h2>
+                <p className="bar-body">Body</p>
+            </div>    
+              
         </section>
     )
 }
